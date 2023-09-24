47a42db7d823a45ca3cfeab090c43388fb948d19
AbdallahSalami <AbdallahSalami@users.noreply.github.com>
2023-09-24 20:56:59 +0300

    step 3

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get("/", (req, res) => {
   res.send("ok");
 });
 
+app.get("/test", (req, res) => {
+  res.status(200).json({ status: 200, message: "ok" });
+});
+
+app.get("/time", (req, res) => {
+  const currentTime = new Date().toTimeString();
+  res.status(200).json({ status: 200, message: currentTime });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
