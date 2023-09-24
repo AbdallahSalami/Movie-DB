6dfb17bbd79bff6dd945aa9b3e470f50c190342e
AbdallahSalami <AbdallahSalami@users.noreply.github.com>
2023-09-24 20:59:02 +0300

    step 4

--- index.js
+++ index.js
@@ -11,8 +11,34 @@ app.get("/test", (req, res) => {
 });
 
 app.get("/time", (req, res) => {
-  const currentTime = new Date().toTimeString();
+  const currentTime = new Date().toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   res.status(200).json({ status: 200, message: currentTime });
+});
+
+// Route to handle /hello/<ID>
+app.get("/hello/:id?", (req, res) => {
+  const { id } = req.params;
+  const message = id ? `Hello, ${id}` : "Hello, World!";
+  res.status(200).json({ status: 200, message });
+});
+
+// Route to handle /search?s=<SEARCH>
+app.get("/search", (req, res) => {
+  const searchQuery = req.query.s;
+  if (searchQuery) {
+    res.status(200).json({ status: 200, message: "ok", data: searchQuery });
+  } else {
+    res
+      .status(500)
+      .json({
+        status: 500,
+        error: true,
+        message: "You have to provide a search",
+      });
+  }
 });
 
 app.listen(PORT, () => {
