f3506b795956299d94178133e55669f72d3d3ea8
AbdallahSalami <AbdallahSalami@users.noreply.github.com>
2023-09-28 02:07:06 +0300

    Step 13

--- index.js
+++ index.js
@@ -18,8 +18,32 @@ const movieSchema = new mongoose.Schema({
 
 const Movie = mongoose.model("Movie", movieSchema);
 
+const users = [
+  { id: 1, username: "John", password: "1234" },
+  { id: 2, username: "Jane", password: "5678" },
+];
+
 // This middleware allows Express to parse incoming JSON requests
 app.use(express.json());
+
+const authenticateUser = (req, res, next) => {
+  const { username, password } = req.headers;
+
+  const user = users.find(
+    (user) => user.username === username && user.password === password
+  );
+
+  if (!user) {
+    res.status(401).json({ status: 401, error: true, message: "Unauthorized" });
+    return;
+  }
+
+  // Attach the authenticated user to the request object for further use
+  req.authenticatedUser = user;
+  next();
+};
+
+app.use("/movies/:id", authenticateUser);
 
 app.get("/", (req, res) => {
   res.send("ok");
@@ -167,6 +191,78 @@ app.put("/movies/:id", async (req, res) => {
     }
   } catch (error) {
     res.status(500).json({ status: 500, error: true, message: error.message });
+  }
+});
+
+// Get all users
+app.get("/users", (req, res) => {
+  res.status(200).json({ status: 200, data: users });
+});
+
+// Get a specific user by ID
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find((user) => user.id === parseInt(id));
+
+  if (user) {
+    res.status(200).json({ status: 200, data: user });
+  } else {
+    res
+      .status(404)
+      .json({ status: 404, error: true, message: "User not found" });
+  }
+});
+
+// Create a new user
+app.post("/users", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(400).json({
+      status: 400,
+      error: true,
+      message: "Please provide username and password for the user.",
+    });
+    return;
+  }
+
+  const id = users.length + 1;
+  const newUser = { id, username, password };
+  users.push(newUser);
+
+  res.status(201).json({ status: 201, data: newUser });
+});
+
+// Update a user by ID
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { username, password } = req.body;
+
+  const user = users.find((user) => user.id === parseInt(id));
+
+  if (user) {
+    user.username = username || user.username;
+    user.password = password || user.password;
+    res.status(200).json({ status: 200, data: user });
+  } else {
+    res
+      .status(404)
+      .json({ status: 404, error: true, message: "User not found" });
+  }
+});
+
+// Delete a user by ID
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const index = users.findIndex((user) => user.id === parseInt(id));
+
+  if (index !== -1) {
+    users.splice(index, 1);
+    res.status(200).json({ status: 200, message: "User deleted successfully" });
+  } else {
+    res
+      .status(404)
+      .json({ status: 404, error: true, message: "User not found" });
   }
 });
 
